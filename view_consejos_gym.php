daab9193c165f63394424cdcf4c09feb21439d4a
djdavid319 <80233864+djdavid319@users.noreply.github.com>
2021-03-07 23:18:45 -0500

    Vista consejos del gym

---
+++ view_consejos_gym.php
@@ -0,0 +1,103 @@
+<!-- Page Content -->
+<div class="container">
+
+<!-- Page Heading -->
+<h1 class="my-4">LOS CONSEJOS DE GYM
+</h1>
+
+<!-- Project One -->
+<div class="row">
+  <div class="col-md-7">
+    <a href="#">
+    <img class="img-fluid" src="<?php echo base_url()?>imagenes/cinco.jpg" alt="">
+    </a>
+  </div>
+  <div class="col-md-5">
+    <h3>SUPLEMENTOS QUE TE AYUDARAN A ADELGAZAR</h3>
+    <p>Está es una lista de los ingredientes más utilizados en el mercado para conseguir adelgazar y perder peso. Son totalmente naturales y los puedes encontrar en tu alimentación, pero aquí ejemplificamos los productos que existen de tales ingredientes.</p>
+    <a class="btn btn-primary"  aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/entra">Entra</a>
+  </div>
+</div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Project Two -->
+<div class="row">
+  <div class="col-md-7">
+    <a href="#">
+    <img class="img-fluid" src="<?php echo base_url()?>imagenes/seis.jpg" alt="">
+    </a>
+  </div>
+  <div class="col-md-5">
+    <h3>TOP DE LAS MEJORES MAQUINAS PARA HACER REMOS  2021</h3>
+    <p>En este listado podrás descubrir las mejores máquinas de remo (top 10 ventas) de nuestra tienda Mundo Fitness y, gracias a la información que te proporcionamos, podrás decidir por ti mismo cuál es la mejor máquina de remo. Podrás ampliar los detalles de cada artículo clicando en “Más información”. Si antes de comprar una máquina de remo para casa quieres dominar la técnica, puedes consultar este artículo.</p>
+    <a class="btn btn-primary"  aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/Entra2">Entra</a>
+  </div>
+</div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Project Three -->
+<div class="row">
+  <div class="col-md-7">
+    <a href="#">
+    <img class="img-fluid" src="<?php echo base_url()?>imagenes/siete.jpg" alt="">
+    </a>
+  </div>
+  <div class="col-md-5">
+    <h3>5 EJERCICIOS PARA MEJORAR Y TRABAJAR LA FLEXIBILIDAD DE PIERNAS</h3>
+    <p>Cuando entrenamos muchas veces tendemos a centrar nuestra atención en ganar masa muscular. No obstante, es importante aumentar la flexibilidad del cuerpo, ya que no hacerlo podría incrementar la posibilidad de sufrir lesiones. Conforme nos hacemos mayores vamos perdiendo flexibilidad, pero hay formas de luchar contra ello. A continuación te proponemos unos cuantos ejercicios de flexibilidad de piernas que te serán muy útiles para tu propósito.</p>
+    <a class="btn btn-primary"  aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/Entra3">Entra</a>
+  </div>
+</div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Project Four -->
+<div class="row">
+
+  <div class="col-md-7">
+    <a href="#">
+    <img class="img-fluid" src="<?php echo base_url()?>imagenes/ocho.jpg" alt="">
+    </a>
+  </div>
+  <div class="col-md-5">
+    <h3>EJERCICOS PARA ELIMINAR LA GRASA </h3>
+    <p>La grasa localizada en la parte inferior de la espalda nunca es bienvenida, especialmente cuando se cierne justo por encima de los pantalones, formando una especie de michelín espantoso. El exceso de grasa en la zona baja de la espalda es a menudo un indicador de exceso de grasa en todo el cuerpo en general y es necesario hacer ejercicios de lumbares.</p>
+    <P>Reducir la grasa de la espalda baja por sí sola no es posible, pero reducir la grasa en general, sí. Combinando un ejercicio cardiovascular regular (mínimo 3 veces por semana) con ejercicios de entrenamiento de fuerza focalizados en trabajar las lumbares, conseguiremos minimizar el exceso de grasa corporal. Perder el exceso de grasa tonificando los músculos de esta zona te ayudará a conseguir una espalda más definida y sexy.</P>
+    <a class="btn btn-primary"  aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/entra4">Entra</a>
+  </div>
+</div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Pagination -->
+<ul class="pagination justify-content-center">
+  <li class="page-item">
+    <a class="page-link" href="#" aria-label="Previous">
+      <span aria-hidden="true">&laquo;</span>
+      <span class="sr-only">atras</span>
+    </a>
+  </li>
+  <li class="page-item">
+  <a class="page-link" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/siguientepag1">1</a>
+  </li>
+  </li>
+  <li class="page-item">
+  <a class="page-link" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/siguientepag1">2</a>
+  </li>
+  <li class="page-item">
+    <a class="page-link" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/siguientepag1">
+      <span aria-hidden="true">&raquo;</span>
+      <span class="sr-only">Siguiente</span>
+    </a>
+  </li>
+</ul>
+
+</div>
+<!-- /.container -->
+
