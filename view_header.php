d243389a6e97ce6d07d722cca6bd79f9709b1264
djdavid319 <80233864+djdavid319@users.noreply.github.com>
2021-03-07 23:12:06 -0500

    vista de la cabeza de la pagina
    en esta vista vamos a encontrar la cabeza de la pagina que seria en este caso el menú que va a aparecer en cada unas de las vista de nuestra pagina.
    Esta configurado con los link de cada vista para que sea fácil el manejo

---
+++ view_header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+            <title>Document</title>
+
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <link rel="stylelesheet" href="css/estilos.css">
+    <div class="container-fluid">
+        <a class="navbar-brand"  aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/index">FITNESS WORLD</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/encuentra">Consejos Gym</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/encuentro">clases grupales</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/encuentri">Tienda</a>
+            </li>
+        </ul>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            
+        </form>
+        
+      </div>
+    </nav>           
+  
+           
+    
+
+
+</head>
+<body>
