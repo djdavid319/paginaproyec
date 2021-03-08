ae68cb111e1ba82c88b241bf6200b3044e0d5183
djdavid319 <80233864+djdavid319@users.noreply.github.com>
2021-03-07 23:32:37 -0500

    Dos vistas tienda y se despliega una segunda pestaña
    En esta vista encontraremos una gran variedad de maquinas para gimnasios donde esta detalladamente que es y el precio de cada una.
    esta distribuida en dos paginas

---
+++ view_tienda.php
@@ -0,0 +1,150 @@
+
+<!-- Page Content -->
+<div class="container">
+
+  <!-- Page Heading -->
+  <h1 class="my-4">LA TIENDA ONLINE Nº 1 DE MAQUINARIA FITNESS
+    <small></small>
+  </h1>
+
+  <div class="row">
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/13.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>BODYTONE DS-06 BICICLETA SPINNING</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$560.000</h2></p>
+          </center>
+          <p class="card-text">Inspirada en el movimiento del ciclismo de carretera y del triatlón, la DS-06 es una bicicleta indoor de gran rendimiento para deportistas gracias a su rueda de inercia trasera.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/14.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>FYTTER RIDER RI-02R BICICLETA SPINNING</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$600.000</h2></p>
+          </center>
+          <p class="card-text">La FYTTER RIDER RI-02R es una bicicleta de biking con resistencia manual y volante de inercia de 13 kg. Su display con pantalla LCD </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/15.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>FYTTER RI-04RNG INDOOR BIKE</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$650.000</h2></p>
+          </center>
+          <p class="card-text">La Bicicleta FYTTER RIDER RI-04R es una bicicleta indoor con una excelente estabilidad. Su cuadro de gran calidad le asegura una mayor rigidez y una transmisión óptima de la fuerza al volante de inercia. Además, sus 18 kg de volante de inercia te garantizan un movimiento uniforme, incluso en las sesiones de entrenamiento más intensas.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/16.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>FYTTER RIDER RI-05R CICLISMO INDOOR</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$700.000</h2></p>
+          </center>
+          <p class="card-text">La Fytter Rider RI-05R es una de las bicicletas de spinning más completas del mercado. Si buscas realizar el entrenamiento de ciclo indoor completo, no lo dudes, esta es tu bicicleta.
+          Con todas las opciones que se muestran en la consola que incorpora, podrás controlar tu entrenamiento y tus rutinas e intentar superarte a diario.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/17.jpeg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>TITANIUM STRENGTH TS1 CICLO INDOOR </a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$750.000</h2></p>
+          </center>
+          <p class="card-text">Diseñada para asegurar el máximo confort, la nueva bicicleta indoor Titanium Strength TS1 presenta una sensación extremadamente realista gracias a su sistema de transmisión de correa, diseñado para garantizar un movimiento fluido y natural que ayuda a la prevención de lesiones.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/18.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>TUNTURI WT85 LEVERAGE PULLER GYM</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$850.000</h2></p>
+          </center>
+          <p class="card-text">El Tunturi WT85 es una herramienta de equipo completa para el entrenamiento de fuerza. Sentadillas, press de hombros, press de banca, bíceps curl, lat pull-down y pull upt: esta polea se convierte en su compañero de entrenamiento.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/19.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>MULTIESTACIÓN WEIDER PRO 8700</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$1.000.000</h2></p>
+          </center>
+          <p class="card-text">La Weider 8700 es un gimnasio compacto por lo que no ocupa toda la habitación pero mantiene las cualidades esenciales para un entrenamiento de calidad. 
+            Tanto las poleas altas como bajas pueden usarse con los accesorios incluidos. La polea alta es perfecta para ejercicios de tracción lateral, extensiones de tríceps y abdominales crunch. La polea baja es mejor para ejercicios de remo sentado y flexiones de bíceps. </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/19.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>BH FITNESS MULTITOWER ST5450</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$2.000.000</h2></p>
+          </center>
+          <p class="card-text">El banco de musculación/Multi Tower ST5450 permite realizar 4 tipos de entrenamiento distintos en el mismo banco. Cada puesto está diseñado para completar un entrenamiento diferente, por lo que es ideal para ejercitar todas las partes de tu cuerpo solo con una máquina.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+
+  <!-- Pagination -->
+  <ul class="pagination justify-content-center">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="atras">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">atras</span>
+          </a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">1</a>
+    <li class="page-item">
+    <a class="page-link" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/siguiente">2</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="<?php echo base_url(); ?>index.php/Welcome/siguiente">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">siguiente</span>
+          </a>
+    </li>
+  </ul>
+
+</div>
+<!-- /.container -->
---
+++ view_tienda_siguiente.php
@@ -0,0 +1,147 @@
+<!-- Page Content -->
+<div class="container">
+
+  <!-- Page Heading -->
+  <h1 class="my-4">LA TIENDA ONLINE Nº 1 DE MAQUINARIA FITNESS 2
+    <small></small>
+  </h1>
+
+  <div class="row">
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/21.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Marcy TSA5762 Sistema Olímpico Half Smith</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$3.000.000</h2></p>
+          </center>
+          <p class="card-text">El TSA5762 cuenta con un nuevo y sorprendente diseño que incorpora el banco y la máquina Smith en un único equipo.
+            Adaptable a discos tanto olímpicos como estándar, este atractivo sistema mitad Smith también incorpora un ejercitador de pierna, pupitre para preacher curl y barra para curl.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/22.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Multiestación Weider Pro 9900</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$3.100.000</h2></p>
+          </center>
+          <p class="card-text">La Multiestación Weider Pro 9900 está equipada con cables de acero con capa de nylon y permite un ejercicio guiado muy seguro sin sensación de distensión, al ser un modelo de multiestación muy compacto. </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/23.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>AFW STRENGTH MULTIESTACIÓN / PRENSA </a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$4.550.000</h2></p>
+          </center>
+          <p class="card-text">La nueva gama de musculación de AFW te permitirá realizar un número prácticamente ilimitado de ejercicios sin tener que salir al exterior. Esta línea de productos ofrece además una de las mejores relaciones calidad-precio del mercado.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/24.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>POWERTEC OLYMPIC BENCH - NEGRA</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$2.700.000</h2></p>
+          </center>
+          <p class="card-text">Proporcionando estabilidad y apoyo , un Multigimnasio, Banco, o un Power Rack de calidad son esenciales para evitar lesiones , sobre todo si pretendes levantar mucho peso. 
+            La marca americana Powertec está respaldada por el mundialmente famoso (y ganador de 4 Mr Olympia) Jay Cutler. </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/25.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>AFW Set de Body Pump de PVC </a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$200.000</h2></p>
+          </center>
+          <p class="card-text">AFW presenta su nuevo set de Body Pump de PVC que incluye un conjunto completo de discos revestidos en PVC de alta calidad y codificados por colores, agarres integrados y una barra de acero revestida de goma para mayor confort.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/26.png" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Tunturi Set de Discos Olímpicos 125 kilos </a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$450.000</h2></p>
+          </center>
+          <p class="card-text">
+                Tunturi es una marca finlandesa con más de 90 años en el Mercado del fitness. 
+                Todos los productos Tunturi Fitness son sometidos a rigurosos análisis y pruebas de laboratorio para comprobar sus funciones técnicas y científicas.
+                El Diseño orientado a las personas, el compromiso con la calidad y la producción de vanguardia hacen que los productos de fuerza y musculación Tunturi sean imbatibles en el mercado de fitness actual.. </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/27.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>Jordan 140kg Olympic Rubber Disc & Bar Set</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$500.000</h2></p>
+          </center>
+          <p class="card-text">AFW presenta el nuevo set de Body Pump de goma al mejor precio!</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/28.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <a>BH FITNESS MULTITOWER ST5450</a>
+          </h4>
+          <center>
+          <h2> <p class="card-text">$485.000</h2></p>
+          </center>
+          <p class="card-text">AFW Set Body Pump de Goma</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+
+  <!-- Pagination -->
+  <ul class="pagination justify-content-center">
+    <li class="page-item">
+      <a class="page-link"aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/encuentri">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">atras</span>
+          </a>
+    </li>
+    <li class="page-item">
+    <a class="page-link" aria-current="page" href="<?php echo base_url(); ?>index.php/Welcome/encuentri">1</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">2</a>
+   
+  </ul>
+
+</div>
+<!-- /.container -->
