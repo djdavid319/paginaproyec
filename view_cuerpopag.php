f35db7f749ccea6b0583b992c642da5d57eb2441
djdavid319 <80233864+djdavid319@users.noreply.github.com>
2021-03-07 23:18:13 -0500

    Este es la vista cuerpo de la pagina
    En esta vista encontraremos nuestra pagina principal donde estará la información de mi pagina y nos va a enviar a  las opciones que están en el menú para explorarlas.
    
    En esta vista encontraremos una imagen con el nombre de la pagina Fitness wolrd , información y  una galería de imágenes.

---
+++ view_cuerpopag.php
@@ -0,0 +1,136 @@
+<!-- Page Content -->
+<div class="container">
+
+  <!-- Portfolio Item Heading -->
+  <h1 class="my-4">Bienvenidos
+    <small>Inicia con Nosotros</small>
+  </h1>
+
+  <!-- Portfolio Item Row -->
+  <div class="row">
+
+    <div class="col-md-8">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/portada.jpg" alt="">
+    </div>
+
+    <div class="col-md-4">
+      <h3 class="my-3">ENCUENTRA TU SEDE MÁS CERCANA EN NEIVA</h3>
+      <p>Aquí te damos entrenamientos intensos que te ayudarán a tonificar y ganar fuerza-resistencia en los músculos. Decide cómo quieres hacerlo y usa tu propio peso corporal o elementos externos.</p>
+      <h3 class="my-3">MEJORA TU SALUD, MEJORA TU VIDA.</h3>
+      <ul>
+        <li>haz gluteos</li>
+        <li>Gana masa muscular</li>
+        <li>Ten esa figura que siempre deseaste</li>
+        <li>Ejercicios para eliminar la grasa de tu espalda</li>
+        <li>Adelgaza haciendo spinning ¿Es posible?</li>
+        <li>Los mejores ejercicios para gemelos</li>
+      </ul>
+    </div>
+
+  </div>
+  <!-- /.row -->
+
+  <!-- Related Projects Row -->
+  <h3 class="my-4">Ver Entrenamiento con nuestros Instrucores.</h3>
+
+  <div class="row">
+
+    <div class="col-md-3 col-sm-6 mb-4">
+      <a href="#">
+            <img class="img-fluid" src="<?php echo base_url()?>imagenes/uno.jpg" alt="">
+          </a>
+    </div>
+
+    <div class="col-md-3 col-sm-6 mb-4">
+      <a href="#">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/dos.jpg" alt="">
+      </a>
+    </div>
+
+    <div class="col-md-3 col-sm-6 mb-4">
+      <a href="#">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/tres.jpg" alt="">
+          </a>
+    </div>
+
+    <div class="col-md-3 col-sm-6 mb-4">
+      <a href="#">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/cuatro.jpg" alt="">
+          </a>
+    </div>
+
+  </div>
+  <!-- /.row -->
+
+</div>
+<!-- /.container -->
+
+<!-- Page Content -->
+<div class="container">
+
+  <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Nuestra Galeria de Imagenes</h1>
+
+  <hr class="mt-2 mb-5">
+
+  <div class="row text-center text-lg-left">
+
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/nueve.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/dies.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/once.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/doce.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/trece.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/catorce.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/quince.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/diesyseis.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/diesysiete.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/diesyocho.jpg" alt="">
+          </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="#" class="d-block mb-4 h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/diesynueve.jpg" alt="">
+          </a>
+    
+    </div>
+  </div>
+
+</div>
+<!-- /.container -->
