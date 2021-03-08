e50517494e7916e4e806b28749629e341f64447f
djdavid319 <80233864+djdavid319@users.noreply.github.com>
2021-03-07 23:31:12 -0500

    vista de las clases grupales
    En esta vista vamos a encontrar una pestaña donde nos va a indicar las clases que se va a tener en el gym.

---
+++ view_clases.php
@@ -0,0 +1,82 @@
+
+<div class="container">
+
+  <!-- Page Heading -->
+  <center>
+  <h1 class="my-4">FW TRENING
+    <small>TRENING</small>
+  </h1>
+  <h3 class="my-4">Hemos creado una amplia selección de planes de formación para ti. Los entrenadores muestran y explican cómo realizar correctamente cada ejercicio.
+  </h3>
+  <center>
+
+  <div class="row">
+    <div class="col-lg-6 mb-4">
+      <div class="card h-100">
+        <img class="img-fluid" src="<?php echo base_url()?>imagenes/6.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+          <h3>HIIT</h3>
+          </h4>
+          <p class="card-text">Este tipo de entrenamiento te ayudará a quemar grasa, mejorar tu condición física y tonificar todo tu cuerpo. Es un complejo de ejercicios que elevarán su pulso, acelerarán los latidos del corazón y la respiración, y luego elevarán sus niveles de endorfinas a un nivel alto. La repetición regular del entrenamiento HIIT, gracias a la alta intensidad, traerá resultados mucho más rápido que el entrenamiento cardiovascular regular.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/7.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+          <h3>CUERPO INFERIOR</h3>
+          </h4>
+          <p class="card-text">Un complejo de ejercicios para fortalecer los músculos inferiores. En esta pestaña encontrarás un entrenamiento dirigido tanto a hombres, que tiene como objetivo fortalecer la musculatura, como a mujeres que quieren adelgazar y fortalecer sus muslos, glúteos y estómago. Este tipo de entrenamiento se combina perfectamente con el entrenamiento de la parte superior del cuerpo y HIIT, luego crea un ciclo de entrenamiento semanal optimizado.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/8.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <h3>PARTE SUPERIOR DEL CUERPO</h3>
+          </h4>
+          <p class="card-text">Un complejo de ejercicios para fortalecer los músculos superiores. Entrenamiento dirigido a hombres que quieran fortalecer los músculos de la espalda, hombros y pecho y para mujeres que quieran mejorar la apariencia de sus brazos y espalda. La combinación del entrenamiento de la parte superior del cuerpo con el entrenamiento de la parte inferior del cuerpo y HIIT le permite construir un ciclo de entrenamiento semanal de alta calidad.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 mb-4">
+      <div class="card h-100">
+        <img class="img-fluid" src="<?php echo base_url()?>imagenes/9.jpg" alt="">  
+        <div class="card-body">
+          <h4 class="card-title">
+          <h3>CUERPO COMPLETO</h3>
+          </h4>
+          <p class="card-text">Gracias a este entrenamiento, fortaleceremos todo el cuerpo y mejoraremos su apariencia. Un efecto secundario del entrenamiento de entrenamiento de cuerpo completo es la quema de grasa al aumentar nuestro pulso durante el ejercicio y la pérdida de calorías después de terminarlos. Este complejo de ejercicios realizados con regularidad nos ayuda a cuidar la salud y el estado físico a cualquier edad.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/10.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+            <h3>EXTERIOR</h3>
+          </h4>
+          <p class="card-text">Este tipo de entrenamiento, gracias a un complejo de ejercicios que involucran a la mayoría de nuestros músculos, nos permitirá fortalecer todo el cuerpo y deshacernos de la grasa innecesaria. Puedes realizar este entrenamiento tanto al aire libre como en casa. El entrenamiento al aire libre es una combinación perfecta con el entrenamiento de la parte superior e inferior del cuerpo.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 mb-4">
+      <div class="card h-100">
+      <img class="img-fluid" src="<?php echo base_url()?>imagenes/11.jpg" alt="">
+        <div class="card-body">
+          <h4 class="card-title">
+          <h3>RODILLO</h3>
+          </h4>
+          <p class="card-text">Esta es una unidad de entrenamiento obligatoria que debe realizarse al menos una vez a la semana. Es el complemento perfecto para cualquier tipo de entrenamiento de fuerza, resistencia y cardio. Gracias al aflojamiento de la fascia y al trabajo sobre la movilidad de nuestro cuerpo, nos preocupamos no solo por nuestra apariencia, sino también por la salud, lo que nos permitirá entrenar con regularidad, minimizando el riesgo de lesiones.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+<!-- /.container -->
